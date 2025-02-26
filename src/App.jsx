0b24dfc9d767c0ba740d6ad463dd5a89a36e6cbe
Anhaia09 <leticia.anhaia09@gmail.com>
2025-02-26 12:04:11 -0300

    :tada: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useRef } from "react";
+import axios from "axios";
+import "./App.css";
+import WeatherInformations from "./components/WeatherInformations/WatherInformations";
+
+function App() {
+  const [weather, setWeather] = useState();
+  const inputRef = useRef();
+
+  async function searchCity() {
+    const city = inputRef.current.value;
+    const key = "b425bfb5c8f4fa3b197a2cc9542b7766";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
+
+    const apiInfo = await axios.get(url);
+    setWeather(apiInfo.data);
+  }
+
+  return (
+    <div className='cointainer'>
+      <h1>DevClub Previsão do Tempo</h1>
+      <input ref={inputRef} type="text" placeholder="Digite o nome da Cidade" />
+      <button onClick={searchCity}>Buscar</button>
+
+      {weather && <WeatherInformations weather={weather}/>}
+    </div>
+  );
+}
+
+export default App;
